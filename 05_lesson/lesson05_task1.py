08a688ca7a627148fc840601226dede69173335e
alhaqain <alhaqain2358@gmail.com>
2025-07-19 17:29:33 +0500

    add lesson 5

---
+++ 05_lesson/lesson05_task1.py
@@ -0,0 +1,19 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
+
+driver.maximize_window()
+driver.get("http://uitestingplayground.com/classattr")
+
+btn_XPATH = ("//button[contains("
+             "concat(' ', normalize-space(@class), ' '), ' btn-primary ')]")
+driver.find_element(By.XPATH, btn_XPATH).click()
+
+
+sleep(5)
